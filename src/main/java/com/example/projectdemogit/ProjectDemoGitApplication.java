3aa86a84087a5d399a83cf7a8d686222630f81e2
dinhphap <macdinhphap123@gmail.com>
2023-10-17 13:29:39 +0700

    main

---
+++ src/main/java/com/example/projectdemogit/ProjectDemoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectdemogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDemoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectdemogit/ProjectDemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectdemogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
